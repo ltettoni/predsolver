6892afc822ec3c74c28b732314cff68a0a13936a
Laurent Tettoni <laurent.tettoni@gmail.com>
2013-06-12 23:07:49 +0200

    Removed comments

--- src/test/java/org/logic2j/predsolver/gd3/domain/dto/core/Committee.java
+++ src/test/java/org/logic2j/predsolver/gd3/domain/dto/core/Committee.java
@@ -1,31 +1,6 @@
-/*
- * Copyright (c) ISO (International Organization for Standardization).
- * 
- * THIS SOFTWARE IS PROVIDED BY ISOurce (ISO Source FOR SOFTWARE DEVELOPMENT AND
- * COLLABORATION) AMONG ISO/CS AND ISO NATIONAL MEMBER BODIES, "AS IS" AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL ISOurce OR ITS CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * For more information on the ISOurce project, please see
- * http://isource.iso.org/
- */
 package org.logic2j.predsolver.gd3.domain.dto.core;
 
 
-/**
- * Sample domain model: a pseudo-entity to test the security API. Also making
- * sure it works with hierarchies of classes. Notice I make this "Proposable"
- * and not its parent class, on purpose.
- *
- * @version $Id: Committee.java,v 1.6 2011-06-29 22:55:08 tettoni Exp $
- */
 public class Committee extends Artefact {
 
     public static interface Predicate2<T0> {
